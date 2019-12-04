be0e6cf3a31d4ac828d0766d09ac1ada58c7394e
mo.ze <mo.ze@icloud.com>
2019-12-03 23:32:09 -0800

    final*7

--- src/component/Hotel.js
+++ src/component/Hotel.js
@@ -4,20 +4,26 @@ import  Model  from "./Model";
 
 // props: name address rev[] price
 function Hotel(props) {
-
+let cardsty={{
+  width: "20rem",
+  height:'30rem',
+  marginRight:"20px"
+}}
   return (
     <>
-      <div className="card" style={{width: "18rem", marginRight:"20px" }}>
-      <img src={props.src} className="card-img-top" alt={"A picture of "+props.obj.name }/>
-        <div className="card-body">
-        <h5 className="card-title">{props.obj.name}</h5>
-          <ul className="card-text">
+      <div className="card" style={cardsty} >
+      <img src={props.src} className="card-img" style={{maxHeight:243}}    alt={"A picture of "+props.obj.name }/>
+        <div className="card-body" style={{paddingLeft: 0}}>
+        <h3 className="card-title" style={{textAlign:"center"}}>{props.obj.name}</h3>
+          <ul>
             <li>Street Address: {props.obj.streetAddress}</li>
             <li>Website: <a href= {props.web}>{props.web}</a></li>
-          
+
           </ul>
+
+
           <Model rev={props.obj.rev} name={props.obj.name}  id={refinenametoid(props.obj.name)}/>
-                
+
         </div>
       </div>
     </>
@@ -27,5 +33,5 @@ function Hotel(props) {
 export default Hotel;
 function refinenametoid(string){
   return string.replace(/\s/g,'')
- 
+
 }
--- src/component/Model.js
+++ src/component/Model.js
@@ -4,16 +4,21 @@ import React from "react";
 function Modal(props){
     var name=props.name;
     var id=props.id;
-    
-   
+
+let btnsty={
+width: "80%",
+position: "absolute",
+bottom: "2%",
+padding: 6,
+marginLeft: "10%"}
     return(
-        
+
         <div>
-            <button type="button"  id={"btn"+name} className="btn btn-primary"
+            <button type="button" style={btnsty} id={"btn"+name} className="btn btn-primary"
             data-toggle="modal" data-target={"#"+id}>
             review
             </button>
-            
+
             <div className="modal fade" id={id} tabIndex="-1" >
             <div className="modal-dialog" >
                 <div className="modal-content">
@@ -28,24 +33,24 @@ function Modal(props){
                         <div className="col-md-10">Review</div>
                         <div className="col-md-2">Rating</div>
                         </div>
-                    {props.rev.map((r ,key)=> 
-                
+                    {props.rev.map((r ,key)=>
+
                     <div className="row" key={key}>
                         <div className="col-md-10">{r.review}</div>
                         <div className="col-md-2">{r.rating}</div>
                     </div>
-                    
-                    
-                    
-                    )} 
-                
+
+
+
+                    )}
+
                 </div>
-               
+
                 </div>
             </div>
             </div>
-            
-            </div> 
+
+            </div>
         );
 }
 export default Modal;
