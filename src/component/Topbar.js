4b32130ec67fe780b41d886d750379a5f3720020
ohzecodes <mo.ze@outlook.com>
2022-12-10 15:48:31 -0800

    topbar remove

--- src/component/Topbar.js
+++ src/component/Topbar.js
@@ -1,18 +1,17 @@
 import React from "react";
 function Topbar(prop) {
-  return (
-    <a href={prop.url}>
-      <div className="alert alert-info alert-dismissible" role="alert">
-        <button className="close" data-dismiss="alert">
-          ×
-        </button>
-        {prop.txt}
-        <span style={{ color: "white", textDecoration: "underline" }}>
-          Click here to learn more
-        </span>
-      </div>
-    </a>
-  );
+  return null;
+  // <a href={prop.url}>
+  //   <div className="alert alert-info alert-dismissible" role="alert">
+  //     <button className="close" data-dismiss="alert">
+  //       ×
+  //     </button>
+  //     {prop.txt}
+  //     <span style={{ color: "white", textDecoration: "underline" }}>
+  //       Click here to learn more
+  //     </span>
+  //   </div>
+  // </a>
 }
 
 export default Topbar;
